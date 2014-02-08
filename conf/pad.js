7803236ce7b0b1de57e0320f2a873fdffaa14bb6
胡家友 <jockhu@anjuke.com>
2014-02-08 18:01:30 +0800

    add pad

---
+++ conf/pad.js
@@ -0,0 +1,94 @@
+/**
+ * @version 1.0
+ * @author Jock
+ *
+ * The main configuration file is some constant,
+ * for the basic configuration of the framework.
+ *
+ *
+ */
+
+
+
+/**
+ * Default port
+ * @const
+ */
+exports.port = 52600; 
+
+/**
+ * Default debug
+ * @const
+ */
+exports.debug = true;
+
+/**
+ * Default enable compression
+ * @const
+ */
+exports.enableCompress = false;
+
+/**
+ * Default version
+ * @const
+ */
+exports.version = ''
+
+/**
+ * Default root path
+ * @const
+ */
+exports.root = '/home/www/release/node/pad/jockjs/libs';
+
+/**
+ * Default user root path
+ * @const
+ */
+exports.userRoot = '/home/www/release/node/pad/jockjs-business-pad'; 
+/**
+ * Default user modules
+ * @const
+ */
+exports.userModules = ['site','pad'];
+
+/**
+ * Default component root path
+ * @const
+ */
+exports.componentRoot = '/home/www/release/node/pad/jockjs-components';
+
+/**
+ * Default user modules
+ * @const
+ */
+exports.componentModules = ['utils','ui','logger'];
+
+/**
+ * Default javascript host
+ * @const
+ */
+exports.jsHost= 'http://SERVERNAME/';
+
+/**
+ * Default javascript path
+ * @const
+ */
+exports.jsPath = 'pjs';
+
+/**
+ * Default style hosts
+ * @const
+ */
+exports.cssHost= 'http://SERVERNAME/';
+
+/**
+ * Default style path
+ * @const
+ */
+exports.cssPath = 'pcss';
+
+/**
+ * Default except -> uglify object property
+ * @const
+ */
+exports.except = ['J','eval'];
